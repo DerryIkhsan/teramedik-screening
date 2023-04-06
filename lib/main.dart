cf67c1f1c2ebfc5f23776772b3d8a3e0974465c9
derry ikhsan sholahuddin <ikhsansholahuddin@gmail.com>
2023-04-07 00:36:58 +0700

    update listview layour

--- lib/main.dart
+++ lib/main.dart
@@ -13,29 +13,29 @@ class TesPage extends StatefulWidget {
 }
 
 class _TesPageState extends State<TesPage> {
-  bool isList = true;
+  bool isGridView = true;
 
   List<Map<String, String>> data = [
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
-    {'nama': 'Rumah Sakit Medika', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 1', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 2', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 3', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 4', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 5', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 6', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 7', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 8', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 9', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 10', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 11', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 12', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 13', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 14', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 15', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 16', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 17', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 18', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 19', 'lokasi': 'Bandung'},
+    {'nama': 'Rumah Sakit Medika 20', 'lokasi': 'Bandung'},
   ];
 
   @override
@@ -47,7 +47,7 @@ class _TesPageState extends State<TesPage> {
           centerTitle: true,
           actions: [
             GestureDetector(
-              child: isList
+              child: isGridView
                   ? Icon(
                       Icons.sort,
                       size: 30,
@@ -57,7 +57,7 @@ class _TesPageState extends State<TesPage> {
                       size: 30,
                     ),
               onTap: () {
-                isList = !isList;
+                isGridView = !isGridView;
                 setState(() {});
               },
             ),
@@ -66,66 +66,23 @@ class _TesPageState extends State<TesPage> {
         ),
         body: Column(
           children: [
-            Expanded(
-              child: Container(
-                margin: EdgeInsets.all(10),
-                child: GridView.count(
-                  crossAxisCount: 2,
-                  children: data
-                      .map((e) => Container(
-                            margin: EdgeInsets.all(10),
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(8),
-                              color: Colors.black12,
-                            ),
-                            child: Column(
-                              children: [
-                                Container(
-                                  height: 130,
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(8),
-                                      topRight: Radius.circular(8),
-                                    ),
-                                    color: Colors.blue,
-                                  ),
-                                ),
-                                Container(
-                                  width: double.infinity,
-                                  margin: EdgeInsets.only(top: 20, left: 20),
-                                  child: Column(
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.start,
-                                    children: [
-                                      Text('${e['nama']}', style: TextStyle(
-                                        fontSize: 16,
-                                        fontWeight: FontWeight.w600,
-                                      ),),
-                                      Text('${e['lokasi']}'),
-                                    ],
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ))
-                      .toList(),
-                ),
-              ),
-            ),
+            SizedBox(height: 10,),
+            isGridView ? GridViewRS(data: data) : ListViewRS(data: data),
             Container(
               padding: EdgeInsets.only(top: 20, right: 20),
+              margin: EdgeInsets.only(top: 10),
               width: double.infinity,
               height: 50,
               color: Colors.blue,
               child: Text(
                 'Ver. 1.2.1',
                 style: TextStyle(
-                  fontWeight: FontWeight.w500,
+                  fontWeight: FontWeight.w800,
                   color: Colors.white,
                 ),
                 textAlign: TextAlign.right,
               ),
-            )
+            ),
           ],
         ),
       ),
@@ -133,10 +90,126 @@ class _TesPageState extends State<TesPage> {
   }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ListViewRS extends StatelessWidget {
+  const ListViewRS({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
 
-  // This widget is the root of your application.
+  final List<Map<String, String>> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ListView(
+        scrollDirection: Axis.vertical,
+        shrinkWrap: true,
+        children: data
+            .map((e) => Container(
+                  height: 200,
+                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  padding: EdgeInsets.only(left: 30, top: 30),
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Stack(
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${e['nama']}',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 10,
+                          ),
+                          Text('${e['lokasi']}'),
+                        ],
+                      ),
+
+                      Positioned(
+                        bottom: 20,
+                        right: 20,
+                        child: Icon(Icons.arrow_forward, size: 30),
+                      ),
+                    ],
+                  ),
+                ))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class GridViewRS extends StatelessWidget {
+  const GridViewRS({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  final List<Map<String, String>> data;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(10),
+        child: GridView.count(
+          crossAxisCount: 2,
+          children: data
+              .map((e) => Container(
+                    margin: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.black12,
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          height: 130,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(8),
+                              topRight: Radius.circular(8),
+                            ),
+                            color: Colors.blue,
+                          ),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          margin: EdgeInsets.only(top: 20, left: 20),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                '${e['nama']}',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              Text('${e['lokasi']}'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ))
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Sizer(builder: (context, orientation, deviceType) {
@@ -145,90 +218,5 @@ class MyApp extends StatelessWidget {
         home: TesPage(),
       );
     });
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
   }
 }
