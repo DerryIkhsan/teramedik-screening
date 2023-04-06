87fd242a617f3b83b29226961e6118176f5baa42
derry ikhsan sholahuddin <ikhsansholahuddin@gmail.com>
2023-04-07 02:57:30 +0700

    update url launcher

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:sizer/sizer.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class DetailScreen extends StatelessWidget {
+class DetailScreen extends StatefulWidget {
   const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  State<DetailScreen> createState() => _DetailScreenState();
+}
+
+class _DetailScreenState extends State<DetailScreen> {
+  void launchMap(String address) async {
+    String query = Uri.encodeComponent(address);
+    Uri googleUrl =
+        Uri.parse("https://www.google.com/maps/search/?api=1&query=$query");
+
+    if (await canLaunchUrl(googleUrl)) {
+      await launchUrl(googleUrl);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -78,7 +94,7 @@ class DetailScreen extends StatelessWidget {
               width: double.infinity,
               color: Colors.amber,
               child: SingleChildScrollView(
-                              child: Column(
+                child: Column(
                   children: [
                     // start icon
                     Container(
@@ -86,48 +102,67 @@ class DetailScreen extends StatelessWidget {
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Container(
-                            width: 70,
-                            height: 70,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              color: Colors.brown,
-                            ),
-                            child: Center(
-                              child: Icon(Icons.phone, size: 30),
-                            ),
-                          ),
-                          Container(
-                            width: 70,
-                            height: 70,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              color: Colors.brown,
-                            ),
-                            child: Center(
-                              child: Icon(Icons.pin_drop, size: 30),
+                          InkWell(
+                            onTap: () {
+                              // ignore: deprecated_member_use
+                              launch("tel://022 1234 1234");
+                            },
+                            child: Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                color: Colors.brown,
+                              ),
+                              child: Center(
+                                child: Icon(Icons.phone, size: 30),
+                              ),
                             ),
                           ),
-                          Container(
-                            width: 70,
-                            height: 70,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(16),
-                              color: Colors.brown,
+                          InkWell(
+                            onTap: () {
+                              launchMap('Kota Bandung');
+                            },
+                            child: Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                color: Colors.brown,
+                              ),
+                              child: Center(
+                                child: Icon(Icons.pin_drop, size: 30),
+                              ),
                             ),
-                            child: Center(
-                              child: Icon(Icons.mail, size: 30),
+                          ),
+                          InkWell(
+                            onTap: () {
+                              // ignore: deprecated_member_use
+                              launch(
+                                  'mailto:derryikhsan@yahoo.co.id?subject=Send From Teramedik Apps');
+                            },
+                            child: Container(
+                              width: 70,
+                              height: 70,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(16),
+                                color: Colors.brown,
+                              ),
+                              child: Center(
+                                child: Icon(Icons.mail, size: 30),
+                              ),
                             ),
                           ),
                         ],
                       ),
                     ),
                     // end icon
-                  
+
                     // title
                     Container(
                       margin: EdgeInsets.symmetric(horizontal: 20),
-                      child: Text('Rumah Sakit Medika 1',
+                      child: Text(
+                        'Rumah Sakit Medika 1',
                         style: TextStyle(
                           fontSize: 26,
                           fontWeight: FontWeight.w600,
@@ -136,20 +171,19 @@ class DetailScreen extends StatelessWidget {
                     ),
                     // end title
 
-                    SizedBox(height: 30,),
+                    SizedBox(
+                      height: 30,
+                    ),
 
                     // description
                     Container(
                       margin: EdgeInsets.symmetric(horizontal: 20),
                       child: Text(
-                          'RS. Medika Dramaga yang berkedudukan di Kota Bogor, yang anggaran dasarnya tertuang dalam Akta Pendirian dengan Akta Notaris, tertanggal 1 Oktober 2010 Nomor 3 Peletakan batu pertama pembangunan RS. Medika Dramaga (RSMD) pada tanggal 11 Agustus 2011. Pada tanggal 10 November 2012 adalah awal dari Operasional RS. Medika Dramaga, yang kemudian menjadi HARI ULANG TAHUN RSMD.',
-                            style: TextStyle(
-                              fontSize: 16,
-                              wordSpacing: 2,
-                              letterSpacing: 1
-                            ),
-                            textAlign: TextAlign.left,
-                          ),
+                        'RS. Medika Dramaga yang berkedudukan di Kota Bogor, yang anggaran dasarnya tertuang dalam Akta Pendirian dengan Akta Notaris, tertanggal 1 Oktober 2010 Nomor 3 Peletakan batu pertama pembangunan RS. Medika Dramaga (RSMD) pada tanggal 11 Agustus 2011. Pada tanggal 10 November 2012 adalah awal dari Operasional RS. Medika Dramaga, yang kemudian menjadi HARI ULANG TAHUN RSMD.',
+                        style: TextStyle(
+                            fontSize: 16, wordSpacing: 2, letterSpacing: 1),
+                        textAlign: TextAlign.left,
+                      ),
                     ),
                     // end description
                   ],
